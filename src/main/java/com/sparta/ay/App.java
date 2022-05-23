826b86a9d515f64e3c237689a44a29f9b4e9bdad
Ahmed Yusuf <ahmedyusuf6195@gmail.com>
2022-05-23 13:56:18 +0100

    Initial Commit

---
+++ src/main/java/com/sparta/ay/App.java
@@ -0,0 +1,29 @@
+package com.sparta.ay;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int timeOfDay = 25;
+        String welcomeMessage = greeting(timeOfDay);
+        System.out.println(welcomeMessage);
+    }
+
+    public static String greeting(int timeOfDay) {
+        String message = "";
+        if (timeOfDay >= 5 && timeOfDay <= 11) {
+            message = ("Good Morning");
+        } else if (timeOfDay >=12 && timeOfDay <= 15) {
+            message = ("Good Afternoon");
+        } else if (timeOfDay >= 16 && timeOfDay <= 20){
+            message = ("Good Evening");
+        } else if (timeOfDay >= 21 && timeOfDay <= 23){
+            message = ("Good Night");
+        } else if (timeOfDay >= 0 && timeOfDay <= 4) {
+            message = ("Good Night");
+        } else
+            message = null;
+
+        return message;
+    }
+}
---
+++ src/test/java/com/sparta/ay/AppTest.java
@@ -0,0 +1,37 @@
+package com.sparta.ay;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    @DisplayName("check that 5 is equal to 5")
+    void simpleTest() {
+        Assertions.assertEquals(true, 5 == 5);
+    }
+    @Test
+    @DisplayName("check that we can return Good Morning")
+    void checkTest() {
+        Assertions.assertEquals("Good Morning", App.greeting(6));
+    }
+
+    @Test
+    @DisplayName("check that we can return Good Afternoon")
+    void checkTest2() {
+        Assertions.assertEquals("Good Afternoon", App.greeting(15));
+    }
+
+    @Test
+    @DisplayName("check that we can return Good Evening")
+    void checkTest3() {
+        Assertions.assertEquals("Good Evening", App.greeting(20));
+    }
+
+    @Test
+    @DisplayName("check valid is equal to null")
+    void checkTest4() {
+        Assertions.assertEquals("Null", App.greeting(25));
+    }
+}
